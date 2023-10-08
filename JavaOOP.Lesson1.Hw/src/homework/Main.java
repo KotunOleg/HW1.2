81bba04ed311b94fc5c1b90a10111516fa128e16
KotunOleg <147324130+KotunOleg@users.noreply.github.com>
2023-10-08 16:50:16 +0300

    Add files via upload

---
+++ JavaOOP.Lesson1.Hw/src/homework/Main.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle tr1 = new Triangle(10,12,17);
+        Triangle tr2 = new Triangle(8,10.7,13);
+        System.out.println(tr1.Area());
+        System.out.println(tr2.Area());
+    }
+}
